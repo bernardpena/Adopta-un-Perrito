8ad761c5915aa89733dd55b48c49e836bc9c70fd
Bernardo <bernardpena@gmail.com>
2024-04-20 12:45:51 -0400

    Footer

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -1,9 +1,13 @@
 
 
-export default function Footer() {
-    return (
-        <div className="piePagina">
-            <p className="autor">Bernardo5 2023. ©Todos los derechos reservados.</p>
+const Footer = () => <footer className="page-footer font-small ">
+    <div className="container text-center text-md-left">
+        <div className="footer-copyright text-center ">© 2024 Copyright:
+            <a href="https://github.com/bernardpena" target="_blank"><p className="autor"> Bernardo5</p></a>
         </div>
-    )
-}
+    </div>
+
+</footer>
+
+export default Footer
+
--- src/componentes/MyCard.jsx
+++ src/componentes/MyCard.jsx
@@ -10,9 +10,6 @@ export default function MyCard({ title, imagen, descripcion, text, color }) {
             <div className="card-body text-light">
                 <h4 className="card-title">{title}</h4>
                 <p className="card-text">{descripcion}</p>
-                {/* <button type="button" className="btn btn-primary">
-                    {text}<span className="badge text-bg-secondary">4</span>
-                </button> */}
                 <span className={color}>{text}</span>
             </div>
         </div>
